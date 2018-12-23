50a3a3a8774ac45707376210801c9ef6783f7d12
Caio27816 <31149173+Caio27816@users.noreply.github.com>
2018-12-23 17:28:45 -0200

    Update gay.js

--- commands/gay.js
+++ commands/gay.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 
     let gay = Math.round(Math.random() * 100);
-    let w = message.mentions.users.first().username || message.author.username;  
+    let w = message.mentions.users.first() || message.author;  
     let gayembed = new Discord.RichEmbed()
         .setColor("#f442d4")
-        .setTitle(`:gay_pride_flag: **Eu acho que ${w} tem ${gay}% de chances de ser gay!** :gay_pride_flag:`);
+        .setTitle(`:gay_pride_flag: **Eu acho que ${w.username} tem ${gay}% de chances de ser gay!** :gay_pride_flag:`);
     message.delete(10);
     return message.channel.send(gayembed);
 };
