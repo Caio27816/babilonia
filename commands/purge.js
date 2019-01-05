4befcca90dc1717556953d72894cc0bac7cd0ec6
Caio27816 <31149173+Caio27816@users.noreply.github.com>
2019-01-05 21:51:50 -0200

    Create purge.js

---
+++ commands/purge.js
@@ -0,0 +1,19 @@
+exports.run = (client, message, args) => {      
+let messagecount = parseInt(args[0]);
+
+        var deletedMessages = -1;
+
+        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
+            messages.forEach(m => {
+                 let hbo = message.mentions.users.first();
+                if (m.author.id == hbo.id) {
+                    m.delete().catch(console.error);
+                    deletedMessages++;
+                }
+            });
+        }).then(() => {
+                if (deletedMessages === -1) deletedMessages = 0;
+                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
+                    .then(m => m.delete(2000));
+        }).catch(console.error);
+}
